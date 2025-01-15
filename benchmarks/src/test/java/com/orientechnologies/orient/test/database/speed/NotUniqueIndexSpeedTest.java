c9ebf3522fda63c361fb5e2a4ffc1e88e0c567f0
Tglman <tglman@tglman.com>
2025-01-15 20:39:42 +0000

    refactor: moved some legacy benchmark logic to specific module

---
+++ benchmarks/src/test/java/com/orientechnologies/orient/test/database/speed/NotUniqueIndexSpeedTest.java
@@ -0,0 +1,60 @@
+package com.orientechnologies.orient.test.database.speed;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.test.database.base.OrientMonoThreadTest;
+import java.util.Date;
+
+/**
+ * @author Andrey Lomakin (a.lomakin-at-orientdb.com)
+ * @since 10/16/13
+ */
+public class NotUniqueIndexSpeedTest extends OrientMonoThreadTest {
+  private ODatabaseDocument database;
+  private int counter;
+  private Date date;
+
+  public NotUniqueIndexSpeedTest() throws Exception {
+    super(50000);
+    date = new Date();
+  }
+
+  @Override
+  public void init() throws Exception {
+    super.init();
+
+    database = new ODatabaseDocumentTx("plocal:notUniqueIndexSpeedTest");
+    if (database.exists()) {
+      database.open("admin", "admin");
+      database.drop();
+    }
+
+    database.create();
+
+    OSchema schema = database.getMetadata().getSchema();
+    OClass testClass = schema.createClass("test");
+    testClass.createProperty("indexdate", OType.DATE);
+    testClass.createIndex("indexdate_index", OClass.INDEX_TYPE.NOTUNIQUE_HASH_INDEX, "indexdate");
+  }
+
+  @Override
+  public void cycle() throws Exception {
+    String fVal = counter + "123456790qwertyASD";
+    counter++;
+
+    database.command(
+        "insert into test (x,    y,    z,    j,    k ,   l,    m,    indexdate), values (?, ?, ?,"
+            + " ?, ?, ?, ?, ?)",
+        fVal,
+        fVal,
+        fVal,
+        fVal,
+        fVal,
+        fVal,
+        fVal,
+        date);
+  }
+}
---
+++ benchmarks/src/test/java/com/orientechnologies/orient/test/internal/index/HashIndexSpeedTest.java
@@ -0,0 +1,65 @@
+package com.orientechnologies.orient.test.internal.index;
+
+import com.orientechnologies.common.test.SpeedTestMonoThread;
+import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.id.ORecordId;
+import com.orientechnologies.orient.core.index.OIndex;
+import com.orientechnologies.orient.core.index.OSimpleKeyIndexDefinition;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import java.util.Random;
+
+/**
+ * @author Andrey Lomakin (a.lomakin-at-orientdb.com)
+ * @since 30.01.13
+ */
+public class HashIndexSpeedTest extends SpeedTestMonoThread {
+  private ODatabaseDocumentInternal databaseDocumentTx;
+  private OIndex hashIndex;
+  private Random random = new Random();
+
+  public HashIndexSpeedTest() {
+    super(5000000);
+  }
+
+  @Override
+  public void init() throws Exception {
+    String buildDirectory = System.getProperty("buildDirectory", ".");
+    if (buildDirectory == null) buildDirectory = ".";
+
+    databaseDocumentTx =
+        new ODatabaseDocumentTx("plocal:" + buildDirectory + "/uniqueHashIndexTest");
+    if (databaseDocumentTx.exists()) {
+      databaseDocumentTx.open("admin", "admin");
+      databaseDocumentTx.drop();
+    }
+
+    databaseDocumentTx.create();
+
+    hashIndex =
+        databaseDocumentTx
+            .getMetadata()
+            .getIndexManagerInternal()
+            .createIndex(
+                databaseDocumentTx,
+                "hashIndex",
+                "UNIQUE_HASH_INDEX",
+                new OSimpleKeyIndexDefinition(OType.STRING),
+                new int[0],
+                null,
+                null);
+  }
+
+  @Override
+  public void cycle() throws Exception {
+    databaseDocumentTx.begin();
+    String key = "bsadfasfas" + random.nextInt();
+    hashIndex.put(key, new ORecordId(0, 0));
+    databaseDocumentTx.commit();
+  }
+
+  @Override
+  public void deinit() throws Exception {
+    databaseDocumentTx.drop();
+  }
+}
